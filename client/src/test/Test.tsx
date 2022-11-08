63224095df7c2080eefcc013ec1e485407f27855
Jaenk-99 <jaehy0429@gmail.com>
2022-11-08 13:59:05 +0900

    make develop branch

--- client/src/test/Test.tsx
+++ client/src/test/Test.tsx
@@ -33,6 +33,7 @@ const Test = () => {
       <div> role : {authStore?.role} </div>
       <div> nickname : {authStore?.nickname} </div>
       <div> accessToken : {store.getState().authStore.accessToken} </div>
+      <div>test</div>
     </Container>
   );
 };
