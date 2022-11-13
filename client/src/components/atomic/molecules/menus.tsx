37345427a46b1f55b1523beb62ba8d8ac8f3d560
ssssksss <ssssksss@gmail.com>
2022-11-14 06:03:23 +0900

    [#56]feat: 회원가입 기능 추가

--- client/src/components/atomic/molecules/menus.tsx
+++ client/src/components/atomic/molecules/menus.tsx
@@ -1,20 +1,50 @@
 import styled from "@emotion/styled";
+import { Button } from "@material-ui/core";
+import { useState } from "react";
 import { MAIN_COLOR } from "../../../commons/color";
 import Text1 from "../atoms/text1";
+import SignUpModal from "../templates/SignUpModal";
 
-export default function Menus(){
-  return(
+export default function Menus() {
+  const [isOpenSignInModal, setIsOpenSignInModal] = useState(false);
+  const [isOpenSignUpModal, setIsOpenSignUpModal] = useState(false);
+
+  const modalSignUpToggleHandler = () => {
+    setIsOpenSignUpModal((prev) => !prev);
+  };
+
+  return (
     <Wrapper>
+      <SignUpModal
+        isOpenModal={isOpenSignUpModal}
+        modalToggleHandler={modalSignUpToggleHandler}
+      />
       <Text1 fontSize="2rem" value="공첵" />
+      <AuthButtonGroup>
+        <Button variant="outlined" onClick={() => setIsOpenSignInModal(true)}>
+          로그인
+        </Button>
+        <Button variant="outlined" onClick={() => setIsOpenSignUpModal(true)}>
+          회원가입
+        </Button>
+      </AuthButtonGroup>
     </Wrapper>
-  )
+  );
 }
 
 export const Wrapper = styled.div`
- width: 100%;
   height: 80px;
   background-color: ${MAIN_COLOR};
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+`;
+const AuthButtonGroup = styled.div`
+  display: flex;
+  gap: 10px;
+
+  button {
+    color: white;
+  }
 `;
