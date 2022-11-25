40ffc4791b48b6bfc93250bf488d05654d9b89db
KimSunWooo <rlatjsdn147@naver.com>
2022-11-25 14:41:34 +0900

    myService data

--- client/pages/myservice/index.tsx
+++ client/pages/myservice/index.tsx
@@ -1,34 +1,46 @@
 import styled from "@emotion/styled";
 import SearchIcon from "@mui/icons-material/Search";
 import CreateIcon from "@mui/icons-material/Create";
-import { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import CommentContainer from "../../src/components/atomic/templates/commentContainer";
-import ChartExample from "../detail/chart";
+import ChartExample from "../../src/components/atomic/organisms/chart";
+import { useRouter } from "next/dist/client/router";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import { getBoardList } from "../../src/api/getBoardList";
 
-const data = [
-  { name: "1", title: "test1", who: "children1", content: "content1" },
-  { name: "2", title: "test2", who: "children2", content: "content2" },
-  { name: "3", title: "test3", who: "children3", content: "content3" },
-  { name: "4", title: "test4", who: "children4", content: "content4" },
-  { name: "5", title: "test5", who: "children5", content: "content5" },
-  { name: "6", title: "test6", who: "children6", content: "content6" },
-];
+// const data = [
+//   { name: "1", title: "test1", who: "children1", content: "content1" },
+//   { name: "2", title: "test2", who: "children2", content: "content2" },
+//   { name: "3", title: "test3", who: "children3", content: "content3" },
+//   { name: "4", title: "test4", who: "children4", content: "content4" },
+//   { name: "5", title: "test5", who: "children5", content: "content5" },
+//   { name: "6", title: "test6", who: "children6", content: "content6" },
+// ];
 
 export default function MyService() {
+  const router = useRouter();
   const [rightSide, setRightSide] = useState("");
   const [content, setContent] = useState("content");
+  const [data, setData] = useState<any[]>();
 
-  const onClickCard = useCallback(
-    (e) => {
-      setRightSide(e.currentTarget.id);
-      setContent("content");
-      console.log(rightSide);
-    },
-    [rightSide]
-  );
+  const onClickCard = useCallback((e) => {
+    setRightSide(e.currentTarget.id);
+    setContent("content");
+  }, []);
+
   const changeContent = useCallback((e) => {
     setContent(e.target.id);
   }, []);
+
+  const onClickWrite = (e: React.MouseEvent<HTMLDivElement>) => {
+    router.push("/create");
+  };
+
+  getBoardList(1).then((res) => {
+    return setData(res);
+  });
+  console.log(data);
   return (
     <>
       <SearchBox>
@@ -36,7 +48,7 @@ export default function MyService() {
         <SearchIcon
           style={{ position: "relative", top: "60px", right: "40px" }}
         />
-        <WriteButton>
+        <WriteButton onClick={onClickWrite}>
           <CreateIcon />
           글쓰기
         </WriteButton>
@@ -50,10 +62,6 @@ export default function MyService() {
               <CardBox>
                 <CardSubTitle>{el.name}</CardSubTitle>
                 <CardContext>{el.content}</CardContext>
-              </CardBox>
-              <CardBox>
-                <CardSubTitle>{el.who}</CardSubTitle>
-                <CardContext>내용</CardContext>
               </CardBox>
               <CardBox>
                 <CardSubTitle>{el.content}</CardSubTitle>
@@ -72,17 +80,19 @@ export default function MyService() {
         </LeftSide>
         <CardDivideLine />
         <RightSide>
-          {rightSide && content == "content" ? (
+          <div>
+            <GraphOption id="content" onClick={changeContent}>
+              내용
+            </GraphOption>
+            <GraphOption id="graph" onClick={changeContent}>
+              투표현황
+            </GraphOption>
+          </div>
+          {content == "content" ? (
             data
               .filter((el) => el.name == rightSide)
               .map((el) => (
                 <RightCard key={el.name}>
-                  <button id="content" onClick={changeContent}>
-                    내용
-                  </button>
-                  <button id="graph" onClick={changeContent}>
-                    그래프
-                  </button>
                   <CardTitle>{el.title}</CardTitle>
                   <CardBox>
                     <CardSubTitle>제목</CardSubTitle>
@@ -101,13 +111,30 @@ export default function MyService() {
               ))
           ) : (
             <>
-              <button id="content" onClick={changeContent}>
-                내용
-              </button>
-              <button id="graph" onClick={changeContent}>
-                그래프
-              </button>
               <ChartExample />
+              <VoteTitle>이 의견에 대해서 어떻게 생각하세요?</VoteTitle>
+              <IconBox>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  좋아요
+                  <ThumbUpIcon style={{ marginTop: "10px" }} />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  별로인거같아요
+                  <ThumbDownIcon style={{ marginTop: "10px" }} />
+                </div>
+              </IconBox>
             </>
           )}
         </RightSide>
@@ -175,6 +202,7 @@ export const RightSide = styled.div`
   width: 40em;
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding: 20px 40px;
 `;
 
@@ -192,6 +220,7 @@ export const RightCard = styled.div`
   padding: 20px;
   border: 2px solid black;
   border-radius: 15px;
+  margin-top: 20px;
 `;
 
 export const CardTitle = styled.div`
@@ -213,4 +242,21 @@ export const CardSubTitle = styled.div`
 
 export const CardContext = styled.div`
   font-size: 1rem;
+`;
+
+export const GraphOption = styled.button`
+  width: 10em;
+`;
+
+export const IconBox = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const VoteTitle = styled.div`
+  font-size: 1.5rem;
+  padding: 10px;
 `;
